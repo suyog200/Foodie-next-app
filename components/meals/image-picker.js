c39c7b1c58670300cdc21d329ec5ab0bbf37be08
suyog200 <suygchari@gmail.com>
2024-06-24 17:20:37 +0530

    component image picker and preview

---
+++ components/meals/image-picker.js
@@ -0,0 +1,58 @@
+"use client";
+import Image from "next/image";
+import { useRef, useState } from "react";
+import classes from "./image-picker.module.css";
+
+export default function ImagePicker({ label, name }) {
+  const [pickedImage, setPickedImage] = useState(); 
+  const imageInput = useRef();
+
+  function handleClick() {
+    imageInput.current.click();
+  }
+
+  function handleImageChange(event) {
+    const file = event.target.files[0];
+    if(!file) {
+        setPickedImage(null);
+        return;
+    }
+
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+        setPickedImage(fileReader.result);
+    };
+    fileReader.readAsDataURL(file);
+  }
+
+  return (
+    <div className={classes.picker}>
+      <label htmlFor={name}>{label}</label>
+      <div className={classes.controls}>
+        <div className={classes.preview}>
+            {!pickedImage && <p>No image picked yet.</p>}
+            {pickedImage && (
+                <Image
+                    src={pickedImage}
+                    alt="Picked image"
+                    fill
+                />
+            )}
+        </div>
+        <input
+          className={classes.input}
+          type="file"
+          id={name}
+          accept="image/png, image/jpeg"
+          name="image"
+          ref={imageInput}
+          onChange={handleImageChange}
+          required
+        />
+        <button className={classes.button} type="button" onClick={handleClick}>
+          Pick an image
+        </button>
+      </div>
+    </div>
+  );
+}
