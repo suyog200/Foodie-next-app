fe298194d2c112ae3e051ef980e074b87016b9b0
suyog200 <suygchari@gmail.com>
2024-06-24 10:31:25 +0530

    added meals item & meals grid. Fixed navlink active links

--- app/meals/share/page.js
+++
@@ -1,7 +0,0 @@
-export default function ShareMealPage() {
-    return (
-        <main>
-            <h1>Share a Meal</h1>
-        </main>
-    );
-}
--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -1,10 +1,12 @@
 import MainHeaderBackground from '@/components/main-header/main-header-background';
+import NavLink from './nav-link';
 import Link from "next/link";
 import Image from "next/image";
 import LogoImg from "../../public/images/logo.png";
 import classes from "./main-header.module.css";
 
 export default function MainHeader() {
+
   return (
     <>
     <MainHeaderBackground />
@@ -20,10 +22,10 @@ export default function MainHeader() {
       <nav className={classes.nav}>
         <ul>
             <li>
-                <Link href="/meals">Browse Meals</Link>
+              <NavLink href="/meals">Browse Meals</NavLink>
             </li>
             <li>
-                <Link href="/community">Foodies Community</Link>
+            <NavLink href="/community">Foodies Community</NavLink>
             </li>
         </ul>
       </nav>
