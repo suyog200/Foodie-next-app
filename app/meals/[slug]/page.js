3d85d36e525be24c4cb299b16c0a17fd96ef3930
suyog200 <suygchari@gmail.com>
2024-06-24 15:45:36 +0530

    added meal details page and error handling

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,7 +1,34 @@
-export default function MealsDetailsPage() {
-    return (
-        <main>
-            <h1>Meals Details</h1>
-        </main>
-    );
+import Image from "next/image";
+import { notFound } from "next/navigation";
+import classes from "./page.module.css";
+import { getMeal } from "@/lib/meals";
+
+export default function MealsDetailsPage({params}) {
+    const meal = getMeal(params.slug);
+
+    if(!meal) {
+        return notFound();
+    }
+    
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p className={classes.instructions} dangerouslySetInnerHTML={{
+            __html: meal.instructions.replace(/\n/g, "<br>")
+        }}></p>
+      </main>
+    </>
+  );
 }
