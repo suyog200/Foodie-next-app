35e736b4f90db69a50884b027d30d557ba477682
suyog200 <suygchari@gmail.com>
2024-06-25 15:49:00 +0530

    added validations for input

---
+++ app/meals/share/error.js
@@ -0,0 +1,10 @@
+'use client';
+
+export default function Error() {
+    return (
+        <main className="error">
+            <h1>Invalid Inputs!</h1>
+            <p>Please check your inputs and try again.</p>
+        </main>
+    )
+}
--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,9 +1,12 @@
 'use client';
+import {useFormState} from 'react-dom'
 import { shareMeal } from '@/lib/actions';
 import ImagePicker from '@/components/meals/image-picker';
+import MealsFormSubmit from '@/components/meals/meal-form-submit';
 import classes from './page.module.css';
 
 export default function ShareMealPage() {
+  const [state, formAction] = useFormState(shareMeal, {message: null});
 
   return (
     <>
@@ -14,7 +17,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -43,8 +46,9 @@ export default function ShareMealPage() {
             ></textarea>
           </p>
           <ImagePicker  label="your image" name="image" />
+          {state.message && <p>{state.message}</p>}
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
